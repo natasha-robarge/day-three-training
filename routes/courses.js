0ea385f900ac7af8757c4371f499eb699aabd865
Natasha Robarge <robarge.natasha7@gmail.com>
2017-10-12 10:09:44 -0500

    Finished creating endpoints for courses

--- routes/courses.js
+++ routes/courses.js
@@ -10,25 +10,49 @@ function getCourses(req, res) {
 }
 
 function createNewCourse(req, res) {
+  const newCourse = {
+    "name": req.body.name,
+    "instructors": req.body.instructors,
+    "students": req.body.students
+  }
 
+  newCourse.save({}, (err, savedCourse) => {
+    if (err) {
+      res.sendStatus(500).send(`Could not save course in database, ${err}`);
+    }
+    res.json(savedCourse);
+  });
 }
 
 function getCourse(req, res) {
-
+  db.Course.findOne({_id: req.params.id}, (err, foundCourse) => {
+    if (err) {
+      res.sendStatus(500).send(`Could not get course, ${err}`);
+    }
+    res.json(foundCourse);
+  });
 }
 
 function updateCourse(req, res) {
-
+  db.Course.findByIdAndUpdate({_id: req.params.id}, (err, updatedCourse) => {
+    res.sendStatus(500).send(`Could not update course, ${err}`);
+  });
+  res.json(updatedCourse);
 }
 
 function removeCourse(req, res) {
-
+  db.Course.findByIdAndRemove({_id: req.params.id}, (err, removedCourse) => {
+    if (err) {
+      res.sendStatus(500).send(`Could not remove course, ${err}`);
+    }
+    res.json(removedCourse);
+  });
 }
 
 module.exports = {
   getCourses: getCourses,
-  getCourse: getCourse,
   createNewCourse: createNewCourse,
+  getCourse: getCourse,
   updateCourse: updateCourse,
   removeCourse: removeCourse
 }
