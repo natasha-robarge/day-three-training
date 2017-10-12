968a89ca332db4941868de23d3ac52125d1a94b9
Natasha Robarge <robarge.natasha7@gmail.com>
2017-10-12 10:19:53 -0500

    Finished creating endpoints for student routes

--- routes/students.js
+++ routes/students.js
@@ -1,23 +1,57 @@
 const db = require(`../models/student`);
 
 function getAllStudents(req, res) {
-
+  db.Student.find({}, (err, foundStudents) => {
+    if (err) {
+      res.sendStatus(500).send(`Could not load students, ${err}`);
+    }
+    res.json(foundStudents);
+  });
 }
 
 function createNewStudent(request, response) {
+  const newStudent = {
+    "firstName": req.body.firstName,
+    "lastName": req.body.lastName,
+    "passing": req.body.passing,
+    "course": req.body.course,
+    "dob": req.body.dob,
+    "email": req.body.email
+  }
 
+  newStudent.save({}, (err, savedStudent) => {
+    if (err) {
+      res.sendStatus(500).send(`Could not save student, ${err}`);
+    }
+    res.json(savedStudent);
+  });
 }
 
 function getStudent(req, res) {
-
+  db.Student.findOne({_id: req.params.id}, (err, foundStudent) {
+    if (err) {
+      res.sendStatus(500).send(`Could not get student, ${err}`);
+    }
+    res.json(foundStudent);
+  });
 }
 
 function updateStudent(req, res) {
-
+  db.Student.findByIdAndUpdate({_id: req.params.id}, (err, updatedStudent) => {
+    if (err) {
+      res.sendStatus(500).send(`Could not update student, ${err}`);
+    }
+    res.json(updatedStudent);
+  });
 }
 
 function removeStudent(req, res) {
-
+  db.Student.findByIdAndRemove({_id: req.params.id}, (err, removedStudent) => {
+    if (err) {
+      res.sendStatus(500).send(`Could not remove student, ${err}`);
+    }
+    res.json(removedStudent);
+  });
 }
 
 module.exports = {
